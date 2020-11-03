c12a109de084ff09b75671523ef07a8e21f582f1
Eric Liu <eric.sa.liu@gmail.com>
2020-11-03 09:30:35 -0800

    added basic player commands and timed pause_in/continue_in commands

--- dstudy/src/main/java/com/github/steventn96/dstudy/DStudy.java
+++ dstudy/src/main/java/com/github/steventn96/dstudy/DStudy.java
@@ -26,6 +26,42 @@ public class DStudy {
 	//data structure to hold all of our commands in one place
 	private static final Map<String, Command> commands = new HashMap<>();
 
+	private static void timerFunc(Timer timer, MessageCreateEvent event, TimerTask task) {
+		//parse input
+		final String content = event.getMessage().getContent();
+		final List<String> command = Arrays.asList(content.split(" "));
+		int timerSeconds;
+		try {
+			timerSeconds = Integer.parseInt(command.get(1));
+		}
+		catch (NumberFormatException e) {
+			event.getMessage().getChannel().block()
+					.createMessage("argument [" + command.get(1) + "] not an integer").block();
+			return;
+		}
+		catch (Exception e) {
+			event.getMessage().getChannel().block()
+					.createMessage("something went wrong [!timer #SECS]").block();
+			return;
+		}
+		// this default_task is invariant so probably can be abstracted out wayyy later
+		TimerTask default_task = new TimerTask() {
+			@Override
+			public void run() {
+				event.getMessage().getChannel().block()
+						.createMessage(timerSeconds + "s timer done!").block();
+			}
+		};
+
+		if (task == null)
+			task = default_task;
+
+		// execute task when timerSeconds * 1000 runs out
+		timer.schedule(task, timerSeconds * 1000);
+		event.getMessage().getChannel().block()
+				.createMessage(timerSeconds + "s timer started").block();
+	}
+
 	private static GatewayDiscordClient initClient(String token) {
 		final GatewayDiscordClient client = DiscordClientBuilder.create(token).build()
 				.login()
@@ -49,39 +85,6 @@ public class DStudy {
 		return client;
 	}
 
-	private static void timerFunc(Timer timer, MessageCreateEvent event) {
-		//parse input
-		final String content = event.getMessage().getContent();
-		final List<String> command = Arrays.asList(content.split(" "));
-		int timerSeconds;
-		try {
-			timerSeconds = Integer.parseInt(command.get(1));
-		}
-		catch (NumberFormatException e) {
-			event.getMessage().getChannel().block()
-					.createMessage("argument [" + command.get(1) + "] not an integer").block();
-			return;
-		}
-		catch (Exception e) {
-			event.getMessage().getChannel().block()
-					.createMessage("something went wrong [!timer #SECS]").block();
-			return;
-		}
-		// this simpleTask is invariant so probably can be abstracted out wayyy later
-		TimerTask simpleTask = new TimerTask() {
-			@Override
-			public void run() {
-				event.getMessage().getChannel().block()
-						.createMessage(timerSeconds + "s timer done!").block();
-			}
-		};
-
-		// execute simpleTask when timerSeconds * 1000 runs out
-		timer.schedule(simpleTask, timerSeconds * 1000);
-		event.getMessage().getChannel().block()
-				.createMessage(timerSeconds + "s timer started").block();
-	}
-
 	private static void initSimpleCommands() {
 		// simple commands here
 		commands.put("ping", event -> event.getMessage()
@@ -90,6 +93,32 @@ public class DStudy {
 		commands.put("swag", event -> event.getMessage()
 				.getChannel().block()
 				.createMessage("Yeet!").block());
+	}
+
+	private static void initPlayerCommands(AudioPlayerManager playerManager, AudioPlayer player, TrackScheduler scheduler) {
+		commands.put("play", event -> {
+			final String content = event.getMessage().getContent();
+			final List<String> command = Arrays.asList(content.split(" "));
+			playerManager.loadItem(command.get(1), scheduler);
+		});
+
+		commands.put("pause", event -> {
+			player.setPaused(true);
+			event.getMessage().getChannel().block()
+					.createMessage("resume with [!resume]").block();
+		});
+
+		commands.put("resume", event -> {
+			if (player.isPaused())
+				player.setPaused(false);
+			else if (player.getPlayingTrack() == null)
+				event.getMessage().getChannel().block()
+						.createMessage("no music paused, play with [!play]").block();
+		});
+
+		commands.put("stop", event ->
+			player.stopTrack()
+		);
 	}
 
 	public static void main(String[] args) {
@@ -105,34 +134,49 @@ public class DStudy {
 		final AudioPlayer player = playerManager.createPlayer();
 		//creates LavaPlayerAudioProvider in next step
 		AudioProvider provider = new LavaPlayerAudioProvider(player);
-		
+
+		final TrackScheduler scheduler = new TrackScheduler(player);
+
+		initPlayerCommands(playerManager, player, scheduler);
+
 		//bot is able to join voice channel to play music
 		commands.put("join", event -> {
-		    final discord4j.core.object.entity.Member member = event.getMember().orElse(null);
-		    if (member != null) {
-		        final VoiceState voiceState = member.getVoiceState().block();
-		        if (voiceState != null) {
-		            final VoiceChannel channel = voiceState.getChannel().block();
-		            if (channel != null) {
-		                // join returns a VoiceConnection which would be required if we were
-		                // adding disconnection features, but for now we are just ignoring it.
-		                channel.join(spec -> spec.setProvider(provider)).block();
-		            }
-		        }
-		    }
-		});
-		
-		//play command and extracts arguments from it
-		final TrackScheduler scheduler = new TrackScheduler(player);
-		commands.put("play", event -> {
-		    final String content = event.getMessage().getContent();
-		    final List<String> command = Arrays.asList(content.split(" "));
-		    playerManager.loadItem(command.get(1), scheduler);
+			final discord4j.core.object.entity.Member member = event.getMember().orElse(null);
+			if (member != null) {
+				final VoiceState voiceState = member.getVoiceState().block();
+				if (voiceState != null) {
+					final VoiceChannel channel = voiceState.getChannel().block();
+					if (channel != null) {
+						// join returns a VoiceConnection which would be required if we were
+						// adding disconnection features, but for now we are just ignoring it.
+						channel.join(spec -> spec.setProvider(provider)).block();
+					}
+				}
+			}
 		});
 
 		// timer stuff
+		// regular timer
 		Timer timer = new Timer();
-		commands.put("timer", event -> timerFunc(timer, event));
+		commands.put("timer", event -> timerFunc(timer, event, null));
+		// pause or resume music after timer; just experimental
+		commands.put("p_in", event ->
+			timerFunc(timer, event, new TimerTask() {
+				@Override
+				public void run() {
+					player.setPaused(true);
+				}
+			})
+		);
+		commands.put("c_in", event ->
+			timerFunc(timer, event, new TimerTask() {
+				@Override
+				public void run() {
+					player.setPaused(false);
+				}
+			})
+		);
+		// hi
 
 		/* create and connect the client -- args[0] has Discord key */
 		final GatewayDiscordClient client = initClient(args[0]);
