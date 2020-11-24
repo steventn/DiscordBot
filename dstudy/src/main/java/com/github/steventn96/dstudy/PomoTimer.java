6bd19348f1519aa81f935762226abb1b1aa6a426
Eric Liu <eric.sa.liu@gmail.com>
2020-11-23 20:21:28 -0800

    ratchet start to init but nothing significant... need to figure out Pair javafx or just work around it lol

--- dstudy/src/main/java/com/github/steventn96/dstudy/PomoTimer.java
+++ dstudy/src/main/java/com/github/steventn96/dstudy/PomoTimer.java
@@ -1,19 +1,63 @@
 package com.github.steventn96.dstudy;
 
+
+import javafx.util.Pair;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.Timer;
+
 public class PomoTimer {
     // would represent an instance of a timer
-    PomoTimer() {
+    private boolean hasStarted;
+    private boolean expired;
+    private Timer internalTimer;
+    private Queue<Pair<Integer, Integer>> taskQueue;
+    private static final Map<Character, Pair<Integer, Integer>> cycles = new HashMap<>();
 
+    /* we'll do the following
+     * short work (25): S
+     * short break (5): B
+     * long work (50): L
+     * long break (10): R
+     */
+    private boolean initializePomo(String cycle) {
+        for (char c: cycle.toCharArray()) {
+            if (!cycles.containsKey(c))
+                return false;
+            taskQueue.add(cycles.get(c));
+        }
+        return true;
+    }
+
+    PomoTimer(String cycle) {
+        hasStarted = false;
+        expired = false;
+        internalTimer = new Timer();
+        cycles.put('S', new Pair<>(1, 25));
+        cycles.put('L', new Pair<>(1, 50));
+        cycles.put('B', new Pair<>(0, 5));
+        cycles.put('R', new Pair<>(0, 10));
+        if (!initializePomo(cycle)) // this should never happen if we have proper input parsing on the user side
+            throw new IllegalArgumentException("bad pomo format");
     }
 
     // start the overall cycle (can only be called once?)
     public boolean startPomo() {
+        if (hasStarted)
+            return false;
+        //start
         return true;
     }
 
-    // end session -- this object should self destruct
+    // end session -- all references hopefully will be maintained properly
     public boolean endPomo() {
+        expired = true;
         return true;
+    }
+
+    public boolean hasEnded() {
+        return expired;
     }
 
     // pause the current timer regardless of study or rest session
@@ -32,7 +76,11 @@ public class PomoTimer {
 
     @Override
     public String toString() {
-        return "Overall Cycle: a b a b a b\n" +
-                "Current progress = 3min/20min";
+        String toret = "";
+        while (!taskQueue.isEmpty()) {
+            Pair<Integer, Integer> next = taskQueue.poll();
+            toret += next.first + ", " + next.second + "/n";
+        }
+        return toret;
     }
 }
